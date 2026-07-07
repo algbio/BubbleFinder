87064b10e86024e1d7d41145b2fe87380e01cb12
Moumard Corentin <corentin@turso02.cs.helsinki.fi>
2026-07-07 02:05:08 +0000

    Add in-memory SPQR index

--- src/util/spqr_index.hpp
+++ src/util/spqr_index.hpp
@@ -1,22 +1,49 @@
-// Records shared by the .spqri reader and BubbleFinder.
+// Read a .spqr file and keep its flat tables in RAM.
 
 #pragma once
 
 #include "util/graph_index.hpp"
 
+#include <algorithm>
+#include <charconv>
 #include <cstddef>
 #include <cstdint>
+#include <fstream>
 #include <istream>
 #include <limits>
 #include <ostream>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <type_traits>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 namespace spqr_index {
 
 #include "util/spqr_index_types.inc"
+
+class IndexedSpqrTree {
+public:
+    IndexedSpqrTree() = default;
+    IndexedSpqrTree(const IndexedSpqrTree&) = delete;
+    IndexedSpqrTree& operator=(const IndexedSpqrTree&) = delete;
+    IndexedSpqrTree(IndexedSpqrTree&&) = default;
+    IndexedSpqrTree& operator=(IndexedSpqrTree&&) = default;
+
+#include "util/spqr_index_storage.inc"
+
+#include "util/spqr_index_runtime.inc"
+
+#include "util/spqr_index_io.inc"
+
+private:
+#include "util/spqr_index_access.inc"
+#include "util/spqr_index_text.inc"
+#include "util/spqr_index_records.inc"
+#include "util/spqr_index_array_helpers.inc"
+};
 
 }
